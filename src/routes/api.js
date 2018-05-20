cb4bd752c37219dcc4387f78b60c658805a2e303
ABondi415 <ABondi415@gmail.com>
2018-05-20 18:42:16 -0400

    Create project structure, initial app, and initial unit tests

---
+++ src/routes/api.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const moment = require('moment');
+
+const router = express.Router();
+
+const logger = require('../modules/logger.service');
+
+router.get('/healthCheck', (request, response, next) => {
+    let loggingId = logger.generateId();
+    let timestamp = moment().format(logger.timestampFormat);
+    logger.info(`health check`);
+
+    next({ response: 'success' });
+});
+
+module.exports = router;
