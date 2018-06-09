7ceb8069a25cfbe7ea534cd4008a8851f13b6cd3
ABondi415 <ABondi415@gmail.com>
2018-06-09 15:46:04 -0400

    Cleanup failed merge on game components.

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -9,27 +9,29 @@ import { Location } from '@angular/common';
 })
 export class GameComponent implements OnInit {
 
-    /** Template reference to the canvas element */
-    @ViewChild('canvasEl') canvasEl: ElementRef;
-  
-    /** Canvas 2d context */
-    private context: CanvasRenderingContext2D;
-    
-    constructor(
-      private route: ActivatedRoute,
-      private location: Location
-    ) { }
+  /** Template reference to the canvas element */
+  @ViewChild('canvasEl') canvasEl: ElementRef;
+
+  /** Canvas 2d context */
+  private context: CanvasRenderingContext2D;
+
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
     this.context = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
     this.draw();
+  }
+
   goBack(): void {
     this.location.back();
   }
 
-    /**
-   * Draws something using the context we obtained earlier on
-   */
+  /**
+ * Draws something using the context we obtained earlier on
+ */
   private draw() {
     var ctx = this.context;
 
@@ -40,25 +42,23 @@ export class GameComponent implements OnInit {
 
     // draw the hero
     var img = new Image();
-    img.onload = function (){
+    img.onload = function () {
       ctx.drawImage(img, 0, 300);
       // this is the x,y location of the image on the canvas
     }
     img.src = '../assets/Smile.png';
-   
+
     //draw the enemy
     var enemy = new Image();
-    enemy.onload = function (){
+    enemy.onload = function () {
       ctx.drawImage(enemy, 200, 200);
       // this is the x,y location of the image on the canvas
     }
     enemy.src = '../assets/Zombie.png';
-   
+
   }
-
-
 }
 
- 
+
 
 
