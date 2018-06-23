af4edbbbe9df1b989b34ad5176fb8c80aa039abe
ABondi415 <ABondi415@gmail.com>
2018-06-22 23:16:01 -0400

    Fix App Component unit test.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ChatWidgetComponent } from './chat-widget/chat-widget.component';
@@ -6,6 +6,9 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from './auth.service';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
@@ -21,20 +24,18 @@ describe('AppComponent', () => {
       ]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'Zombie Task Scheduler'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('Zombie Task Scheduler');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Zombie Task Scheduler!');
+  });
+
+  it('should create the app', async(() => {
+    expect(component).toBeTruthy();
+  }));
+
+  it(`should have as title 'Zombie Task Scheduler'`, async(() => {
+    expect(component.title).toEqual('Zombie Task Scheduler');
   }));
 });
