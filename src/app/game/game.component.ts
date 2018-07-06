091a9245a2365afc53ad1113e955a9ae52d54633
Kristin Gower <kristin_g@verizon.net>
2018-07-06 15:36:08 -0400

    Converted game code to EaselJS

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,9 @@ import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+// CreateJS module for game (EaselJS)
+import * as createjs from 'createjs-module';
+
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
@@ -21,42 +24,44 @@ export class GameComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    //get the canvas element from the html
     this.context = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
     this.draw();
   }
 
-  goBack(): void {
-    this.location.back();
-  }
-
-  /**
- * Draws something using the context we obtained earlier on
- */
+  //draw on the canvas
   private draw() {
-    var ctx = this.context;
+    //create a stage on the canvas
+    var stage = new createjs.Stage(this.canvasEl.nativeElement as HTMLCanvasElement);
 
-    //draw text
-    ctx.font = "20px Georgia";
-    ctx.strokeText("This is the game canvas", 0, 20);
-
-
-    // draw the hero
-    var img = new Image();
-    img.onload = function () {
-      ctx.drawImage(img, 0, 300);
-      // this is the x,y location of the image on the canvas
+    //draw the hero image
+    var heroImg = new Image();
+    heroImg.src = '../assets/Smile.png';
+ 
+    heroImg.onload = function() {
+      var heroBitmap = new createjs.Bitmap('../assets/Smile.png');
+      heroBitmap.x = 10;
+      heroBitmap.y = 150;
+      stage.addChild(heroBitmap);
+      stage.update()
     }
-    img.src = '../assets/Smile.png';
 
-    //draw the enemy
-    var enemy = new Image();
-    enemy.onload = function () {
-      ctx.drawImage(enemy, 200, 200);
-      // this is the x,y location of the image on the canvas
+    //draw the enemy image
+    var enemyImg = new Image()
+    enemyImg.src ='../assets/Zombie.png';
+
+    enemyImg.onload = function() {
+      var enemyBitmap = new createjs.Bitmap(enemyImg);
+      enemyBitmap.x = 700;
+      enemyBitmap.y = 150;
+      stage.addChild(enemyBitmap);
+      stage.update()
     }
-    enemy.src = '../assets/Zombie.png';
 
+    //update the stage
+    stage.update();
   }
+
 }
 
 
