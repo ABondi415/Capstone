32741a215477c33a9e6001c167878f916fd38a97
Kristin Gower <kristin_g@verizon.net>
2018-07-06 18:50:39 -0400

    Update user score when task is completed.

--- src/app/task-details/task-details.component.ts
+++ src/app/task-details/task-details.component.ts
@@ -2,19 +2,26 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { HttpService } from '../http.service';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
+import { AuthService } from '../auth.service';
+import { User } from '../model/user';
+
 import { Task } from '../model/task';
 
 @Component({
   selector: 'app-task-details',
   templateUrl: './task-details.component.html',
-  styleUrls: ['./task-details.component.css']
+  styleUrls: ['./task-details.component.css'],
+  providers: [HttpService, AuthService]
 })
 export class TaskDetailsComponent implements OnInit {
   task: Task;
+  taskUser = new User(null, null, null, null, null);
 
   constructor(public dialogRef: MatDialogRef<TaskDetailsComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private httpService: HttpService) { }
+    private httpService: HttpService,
+    private authService: AuthService
+  ) { }
 
   ngOnInit() {
     this.task = new Task(this.data.task.id,
@@ -27,9 +34,32 @@ export class TaskDetailsComponent implements OnInit {
       this.data.task.sprite,
       false,
       this.data.task.userId);
+
+
+      // get the userdata
+      if(this.authService.isAuthenticated){
+        let currentUserId = JSON.parse(localStorage.getItem('currentUser')).userId;
+        this.taskUser.userId = currentUserId;
+        this.httpService.getOrCreateUser(this.taskUser).subscribe(user => this.taskUser = user);
+       }
+
   }
 
   onUpdateClick(): void {
+
+    if (this.data.task.taskCompleted != this.task.taskCompleted) {
+      if (this.task.taskCompleted){
+        this.addPoints();
+      }
+      else {
+        this.removePoints();
+      }
+    }
+    this.updateTaskInfo();
+  }
+
+
+updateTaskInfo(){
     this.httpService.updateTask(this.task).subscribe(task => {
       this.data.task.id = task.id;
       this.data.task.dueDate = task.dueDate;
@@ -49,4 +79,20 @@ export class TaskDetailsComponent implements OnInit {
   onCancelClick(): void {
     this.dialogRef.close();
   }
+
+  addPoints() {
+    //TBD:  generate a good-job message here...
+    this.taskUser.score = this.taskUser.score + 10;
+    this.updateUserScore();
+  }
+
+  removePoints() {
+    this.taskUser.score = this.taskUser.score - 10;
+    this.updateUserScore();
+  }
+
+  updateUserScore() {
+    this.httpService.updateUser(this.taskUser).subscribe(user => this.taskUser);
+  }
+
 }
