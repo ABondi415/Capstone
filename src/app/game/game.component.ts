fc660026f34472b1d4188cc7551c93fd44f69e5b
Kristin Gower <kristin_g@verizon.net>
2018-07-06 16:44:53 -0400

    Show user name and score on game componet

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,6 +1,10 @@
 import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { HttpService } from '../http.service';
+import { AuthService } from '../auth.service';
+import { Task } from '../model/task';
+import { User } from '../model/user';
 
 // CreateJS module for game (EaselJS)
 import * as createjs from 'createjs-module';
@@ -8,9 +12,16 @@ import * as createjs from 'createjs-module';
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.css']
+  styleUrls: ['./game.component.css'],
+  providers: [HttpService, AuthService]
 })
-export class GameComponent implements OnInit {
+export class GameComponent implements OnInit { 
+
+  // set up an array to hold task data
+  taskList: Array<Task> = [];
+
+  //set up a gameUser
+  gameUser = new User(null, null, null, null, null);
 
   /** Template reference to the canvas element */
   @ViewChild('canvasEl') canvasEl: ElementRef;
@@ -20,13 +31,26 @@ export class GameComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private httpService: HttpService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
+
+    // get the user and task data
+    if(this.authService.isAuthenticated){
+      let currentUserId = JSON.parse(localStorage.getItem('currentUser')).userId;
+      this.httpService.getUserTasks(currentUserId).subscribe(tasks =>  this.taskList = tasks);
+      this.gameUser.userId = currentUserId;
+      this.httpService.getOrCreateUser(this.gameUser).subscribe(user => this.gameUser = user);
+    }
+
+
     //get the canvas element from the html
     this.context = (this.canvasEl.nativeElement as HTMLCanvasElement).getContext('2d');
     this.draw();
+
   }
 
   //draw on the canvas
