5836edfa22e0b4b906a7681d402e78a47c9eaa95
Marc Williams <Marc@Marcs-MacBook-Pro.local>
2018-07-29 15:38:52 -0400

    implementation of point reduction for overdue task items

--- src/app/model/task.ts
+++ src/app/model/task.ts
@@ -2,6 +2,7 @@ export class Task {
   constructor(
     public id: string,
     public dueDate: Date,
+    public penaltyDate: Date,
     public description: string,
     public taskPriority: boolean,
     public taskDetail: string,
